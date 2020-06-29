9bcb3bc4cc4ad8db4aa9e5df76ff90a72b88b124
Cristian Vargas <vargascristian@americana.edu.co>
2020-06-29 09:52:45 -0500

    proyecto base spring-boot

---
+++ src/main/java/report/OpenReportApiApplication.java
@@ -0,0 +1,13 @@
+package report;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenReportApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenReportApiApplication.class, args);
+	}
+
+}
